0c14727ef10de16f22a5430ea68ec44801774363
DevOps Agent <devops@docbridge.com>
2026-06-27 19:30:47 +0530

    Enhance infrastructure deployment stability

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -306,9 +306,11 @@ data "azurerm_client_config" "current" {}
 
 
 resource "azurerm_role_assignment" "monitoring_contributor" {
-  scope                = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.resource_group_name}"
-  role_definition_name = "Monitoring Contributor"
-  principal_id         = data.azurerm_client_config.current.object_id
+  name                             = "3dc496b0-d792-4ef4-90b9-06f120692c7a"
+  scope                            = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.resource_group_name}"
+  role_definition_name             = "Monitoring Contributor"
+  principal_id                     = data.azurerm_client_config.current.object_id
+  skip_service_principal_aad_check = true
 }
 
 
